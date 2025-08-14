283c4e0921162f55d5210db19a52b2b15ceeb14a
IstoMan <suhasnaraparaju@gmail.com>
2025-08-14 11:18:00 +0530

    added the wpm

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -11,10 +12,15 @@ import (
 
 type Model struct {
 	sentence     string
-	input        string
+	typedText    string
 	correctStyle lipgloss.Style
 	normalStyle  lipgloss.Style
 	wrongStyle   lipgloss.Style
+	helpStyle    lipgloss.Style
+	startTime    time.Time
+	isOver       bool
+	hasStarted   bool
+	wpm          float64
 }
 
 func InitialModel() Model {
@@ -22,18 +28,28 @@ func InitialModel() Model {
 		Align(lipgloss.Center)
 
 	cs := lipgloss.NewStyle().
-		Inherit(ns).
+		Align(lipgloss.Center).
 		Foreground(lipgloss.Color("#30fc03"))
 
-	ws := lipgloss.NewStyle().Inherit(ns).
+	ws := lipgloss.NewStyle().
+		Align(lipgloss.Center).
 		Foreground(lipgloss.Color("#fc0324"))
 
+	hs := lipgloss.NewStyle().
+		Align(lipgloss.Bottom).
+		Italic(true).
+		Faint(true)
+
 	return Model{
-		sentence:     "The quick brown fox jumps over the lazy dog.",
-		input:        "",
+		sentence:     "Hello World",
+		typedText:    "",
 		correctStyle: cs,
 		normalStyle:  ns,
 		wrongStyle:   ws,
+		helpStyle:    hs,
+		isOver:       false,
+		hasStarted:   false,
+		wpm:          0,
 	}
 }
 
@@ -50,21 +66,34 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c":
 			return m, tea.Quit
 		case "backspace":
-			if len(m.input) > 0 {
-				m.input = m.input[:len(m.input)-1]
+			if len(m.typedText) > 0 {
+				m.typedText = m.typedText[:len(m.typedText)-1]
 			}
 		case "alt+backspace":
-			if len(m.input) > 0 {
-				for i := len(m.input) - 1; i > 0; i-- {
-					if string(m.input[i]) == " " {
-						m.input = m.input[:i+1]
+			if len(m.typedText) > 0 {
+				for i := len(m.typedText) - 1; i > 0; i-- {
+					if string(m.typedText[i]) == " " {
+						m.typedText = m.typedText[:i+1]
 						break
 					}
 				}
 			}
 		default:
-			if len(m.input) < len(m.sentence) {
-				m.input += string(msg.Runes)
+			if len(m.typedText) < len(m.sentence) {
+				if !m.hasStarted {
+					m.startTime = time.Now()
+					m.hasStarted = true
+				}
+
+				m.typedText += string(msg.Runes)
+
+				if m.sentence == m.typedText {
+					m.isOver = true
+					elapsed := time.Since(m.startTime).Minutes()
+
+					words := float64(len(m.sentence)) / 5
+					m.wpm = words / elapsed
+				}
 			}
 		}
 	}
@@ -74,15 +103,31 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	var ui strings.Builder
-	ui.WriteString(m.normalStyle.Render(m.sentence))
 
-	for i, char := range m.input {
-		if string(char) == string(m.sentence[i]) {
-			ui.WriteString(m.correctStyle.Render(string(char)))
+	if m.isOver {
+		congrats := lipgloss.NewStyle().Foreground(lipgloss.Color("#30fc03")).Align(lipgloss.Center).Border(lipgloss.RoundedBorder(), true)
+		s := fmt.Sprintf("Your final wpm is: %1.f", m.wpm)
+		ui.WriteString(congrats.Render(s))
+		ui.WriteString("\n\n")
+		ui.WriteString(m.helpStyle.Render("ctrl+c to exit"))
+		return ui.String()
+	}
+
+	for i, char := range m.sentence {
+		if i < len(m.typedText) {
+			if m.typedText[i] == byte(char) {
+				ui.WriteString(m.correctStyle.Render(string(char)))
+			} else {
+				ui.WriteString(m.wrongStyle.Render(string(char)))
+			}
 		} else {
-			ui.WriteString(m.wrongStyle.Render(string(char)))
+			ui.WriteString(m.normalStyle.Render(string(char)))
 		}
 	}
+
+	ui.WriteString("\n\n")
+
+	ui.WriteString(m.helpStyle.Render("ctrl+c to exit"))
 
 	return ui.String()
 }
